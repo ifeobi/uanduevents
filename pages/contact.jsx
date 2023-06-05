9c3029479784d794855ef3d2ac01156d871775cb
abc <abc@example.com>
2023-06-05 14:56:37 +0100

    contact us socials

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import fb from "../public/assets/images/fbb.png";
 import bg from "../public/assets/images/contactbg.png";
-import snap from "../public/assets/images/snap.png";
 import InstagramIcon from "@mui/icons-material/Instagram";
-import PinterestIcon from "@mui/icons-material/Pinterest";
+import tiktok from "../public/assets/images/tiktok.png";
 import Image from "next/image";
 import location from "../public/assets/images/location.png";
 import mail from "../public/assets/images/mail.png";
@@ -32,18 +31,27 @@ function Contact() {
               <Image src={mail} className="mail" />
               <p>uandudesign@gmail.com</p>
               <div className="footer-social contact">
-                <div className="socials">
+              <div className="socials">
                   <div>
-                    <InstagramIcon style={{ color: "white" }} />
+                    <a
+                      href="https://www.instagram.com/uandudesigns/"
+                      target="_blank"
+                    >
+                      <InstagramIcon style={{ color: "white" }} />
+                    </a>
                   </div>
                   <div>
-                    <Image src={fb} alt="" />
+                    <a
+                      href="https://www.facebook.com/profile.php?id=100069349773537"
+                      target="_blank"
+                    >
+                      <Image src={fb} alt="" />
+                    </a>
                   </div>
                   <div>
-                    <Image src={snap} alt="" />
-                  </div>
-                  <div>
-                    <PinterestIcon style={{ color: "white" }} />
+                    <a href="https://vm.tiktok.com/ZTREdsYmy/" target="_blank">
+                      <Image src={tiktok} alt="" className="tiktok" />
+                    </a>
                   </div>
                 </div>
                 <h3>FOLLOW US</h3>
